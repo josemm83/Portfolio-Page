495bdaba84fd20e22a6666f227cc3336f5536ba9
josemm83 <josemmancha@gmail.com>
2020-02-10 21:31:18 -0600

    updating homework files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,4 +3,9 @@ $(document).ready(function(){
     $(".sidenav").sidenav();
     $(".materialboxed").materialbox();
     $(".parallax").parallax();
+    $(".tabs").tabs();
+    $(".datepicker").datepicker({
+        disableWeekends: true
+    });
+    $(".scrollspy").scrollSpy();
 });
